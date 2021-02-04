40d3c8b46038845e624733ce6ffa5b71c6803980
julioportellah <julioportellah@gmail.com>
2021-02-04 19:11:46 +0000

    JPortella, adding jsonify

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
 	"""
 	return "<p>Hello World, yay!</p>"
 
+@app.route('/name/<value>')
+def name(value):
+	val = {"value": value}
+	return jsonify(val)
+
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)	
 	pass
