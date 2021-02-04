b2f0f610c1f17906b95eebbe93ec91174b0ced85
julioportellah <julioportellah@gmail.com>
2021-02-04 14:20:39 -0500

    Update main.py
    Adding the html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,10 +10,22 @@ def main():
 	"""
 	return "<p>Hello World, yay!</p>"
 
+
 @app.route('/name/<value>')
 def name(value):
 	val = {"value": value}
 	return jsonify(val)
+
+
+@app.route('/html')
+def html():
+    	"""Returns some custom HTML"""
+    	return """
+    	<title>This is a Hello World World Page</title>
+    	<p>Hello</p>
+    	<p><b>World</b></p>
+    	"""
+
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)	
