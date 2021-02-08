f063e7e95549552ab8559a702cfdbf490774770c
julioportellah <julioportellah@gmail.com>
2021-02-08 10:36:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def html():
     	return """
     	<title>This is a Hello World World Page</title>
     	<p>Hello</p>
-    	<p><b>World</b></p>
+    	<p><b>World!!!</b></p>
+    	<p><b>Hi</b></p>
     	"""
 @app.route('/pandas')
 def pandas_sugar():
