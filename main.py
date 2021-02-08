b30ddeccc48543ddf3041cb03d8f4c6825b443a6
julioportellah <julioportellah@gmail.com>
2021-02-08 02:17:08 -0500

    Update main.py
    Adding more functions after the main correction was done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +8,21 @@ def main():
 	"""
 	The hellow world!
 	"""
-	return "<p>Hello World, yay!</p>"
+	return "<p>Hello World, my name is Julio!</p>"
+
+@app.route('/name/<value>')
+def name(value):
+    	val = {"value": value}
+    	return jsonify(val)
+
+@app.route('/html')
+def html():
+    	"""Returns some custom HTML"""
+    	return """
+    	<title>This is a Hello World World Page</title>
+    	<p>Hello</p>
+    	<p><b>World</b></p>
+    	"""
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)	
