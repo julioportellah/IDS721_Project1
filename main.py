cd85dd61537c5f878a36d11570c00818dcc5f12b
julioportellah <julioportellah@gmail.com>
2021-02-08 01:41:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello world, this is the project 1 and template for further projects that integrate gcloud'
 
-"""
 @app.route('/name/<value>')
 def name(value):
     val = {"value": value}
     return jsonify(val)
 
-"""
 @app.route('/html')
 def html():
     """Returns some custom HTML"""
@@ -31,7 +29,6 @@ def html():
 def pandas_sugar():
     df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
     return jsonify(df.to_dict())
-
 
 
 if __name__ == '__main__':
