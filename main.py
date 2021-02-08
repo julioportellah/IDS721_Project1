0b9bb5c97a5f86f170f84ff46ce12a7d8af11246
julioportellah <julioportellah@gmail.com>
2021-02-08 02:19:56 -0500

    Update main.py
    adding pandas

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def html():
     	<p>Hello</p>
     	<p><b>World</b></p>
     	"""
+@app.route('/pandas')
+def pandas_sugar():
+    	df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    	return jsonify(df.to_dict())
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)	
